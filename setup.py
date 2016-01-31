1f59155033d0f1d9da9a0014f60ff7d9a2db566a
YuviPanda <yuvipanda@gmail.com>
2016-01-30 20:52:42 -0800

    Add setup.py
    Change-Id: I4c4c68c71b52b8781ee10ca5546d3acec6c1be4f

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='debimgbuilder',
+    version='0.1',
+    description='Easily extensible builder for Debian based docker images',
+    url='http://github.com/wikimedia/operations-docker-images-debian',
+    author='Yuvi Panda',
+    author_email='yuvipanda@riseup.net',
+    license='Apache 2',
+    packages=['debimgbuilder'],
+)
